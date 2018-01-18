9a645515fa7d156b702f3049b5a1da03f491bd49
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-18 05:27:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+happyHolidays();
+//////////////////////////////////////////////////////////////////////
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+happyHolidaysTo("Inom");
+/////////////////////////////////////////////////////////////////////
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+happyHolidayTo("holidays Inom");
+/////////////////////////////////////////////////////////////////////
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
+holidayCountdown(20, "Mother's Day");
