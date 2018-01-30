ad870cd5a69b1b7517db2b3d1dacc692f690d98d
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-30 02:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
